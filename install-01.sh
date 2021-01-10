18a0121709c24df9a59c3454aa820249bdf058ea
FABIO JUNIOR RIBEIRO <37555641+frib3iro@users.noreply.github.com>
2021-01-09 23:55:36 -0300

    Add files via upload

--- install-01.sh
+++ install-01.sh
@@ -19,34 +19,24 @@ C='\033[0;36m'
 # Fechamento das cores no script
 F='\033[0m'
 # Seta utilizada no inicio das frases
-S='\e[32;1m>>>\e[m'
+S='\e[32;1m[+]\e[m'
 #----------------------------------------------------------------------
-
-# Para a maquina_virtual
-MaquinaVirtual(){
-    (echo g; echo n; echo ""; echo ""; echo +512M; echo t; echo 1; echo n; echo ""; echo ""; echo ""; echo w) | fdisk ${disco}
-}
-
-# Para a máquina real
-MaquinaReal(){
-    (echo d; echo ""; echo d; echo ""; echo g; echo n; echo ""; echo ""; echo +512M; echo t; echo 1; echo n; echo ""; echo ""; echo ""; echo w) | fdisk ${disco}
-}
 
 # Tela de boas vindas
 clear
 echo -e "${S} ${C}Bem vindo a primeira parte da instalação do Arch Linux no modo UEFI${F}"
-sleep 2
+sleep 1
 
 # Atualizando o relógio do sistema
 echo
 echo -e "${S} ${C}Atualizando o relógio do sistema${F}"
-sleep 2
+sleep 1
 timedatectl set-ntp true
 
 # Listando os discos
 echo
 echo -e "${S} ${C}Listando os discos${F}"
-sleep 2
+sleep 1
 lsblk -l | grep disk
 
 # Informando o nome do seu disco
@@ -54,43 +44,23 @@ echo
 echo -en "${S} ${C}Informe o nome do seu disco:${F} "
 read disco
 disco=/dev/${disco}
-echo
-echo -e "${S} ${C}Digite${F} ${R}[ 1 ]${F} ${C}para iinstalar o sistema na máquina virtual${F}"
-echo -e "${S} ${C}Digite${F} ${R}[ 2 ]${F} ${C}para instalar o sistema na máquina real${F}"
-echo
-echo -en "${S} ${Y}Digite sua resposta:${F} "
-read resposta
 
-if [ "$resposta" -eq 1 ]
-then
-    echo
-    echo -e "${S} ${C}Iniciando particionamento na máquina virtual${F}"
-    sleep 2
-    MaquinaVirtual
-
-elif [ "$resposta" -eq 2 ]
-then
-    echo
-    echo -e "${S} ${C}Iniciando particionamento na máquina real${F}"
-    sleep 2
-    MaquinaReal
-else
-    echo
-    echo -e "${S} ${R}Resposta inválida!${F}"
-    exit 1
-fi
+echo
+echo -e "${S} ${C}Iniciando particionamento do disco${F}"
+sleep 1
+(echo d; echo ""; echo d; echo ""; echo g; echo n; echo ""; echo ""; echo +512M; echo t; echo 1; echo n; echo ""; echo ""; echo ""; echo w) | fdisk ${disco}
 
 # Formatando partições
 echo
 echo -e "${S} ${C}Formatando as partições${F}"
-sleep 2
+sleep 1
 mkfs.fat -F32 ${disco}1
 mkfs.ext4 ${disco}2
 
 # Montando partições
 echo
 echo -e "${S} ${C}Montando as partições${F}"
-sleep 2
+sleep 1
 mount ${disco}2 /mnt
 mkdir -p /mnt/boot/efi
 mount ${disco}1 /mnt/boot/efi
@@ -98,30 +68,30 @@ mount ${disco}1 /mnt/boot/efi
 # Listando partições
 echo
 echo -e "${S} ${C}Conferindo as partições${F}"
-sleep 2
+sleep 1
 lsblk ${disco}
 
 # Instalando os pacotes base
 echo
 echo -e "${S} ${C}Instalando os pacotes base${F}"
-sleep 2
-pacstrap /mnt base base-devel linux linux-firmware vim intel-ucode
+sleep 1
+pacstrap /mnt base base-devel linux linux-firmware intel-ucode
 
 # Gerando o fstab
 echo
 echo -e "${S} ${C}Gerando o fstab${F}"
-sleep 2
+sleep 1
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Copiando o script archinstall-02.sh para /mnt
 echo
 echo -e "${S} ${C}Copiando o script install-02.sh para /mnt${F}"
-sleep 2
+sleep 1
 cp install-02.sh /mnt
 
 # Iniciando arch-chroot
 echo
 echo -e "${S} ${C}Iniciando arch-chroot${F}"
-sleep 2
+sleep 1
 arch-chroot /mnt ./install-02.sh
 
