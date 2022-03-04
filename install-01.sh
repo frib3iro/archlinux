e61751552e4e3299f79f375b4fa7687884749010
FABIO JUNIOR RIBEIRO <37555641+frib3iro@users.noreply.github.com>
2022-03-04 20:56:19 -0300

    Add files via upload

--- install-01.sh
+++ install-01.sh
@@ -21,28 +21,28 @@ S='\e[32;1m[+]\e[m'
 
 # Tela de boas vindas
 clear
-echo -e "${S} ${C}Bem vindo a primeira parte da instalação do Arch Linux UEFI${F}"
+echo -e "${S} ${C}Bem vindo a primeira parte da instalação do Arch Linux UEFI/GPT${F}"
 
 # Definir a fonte do ambiente live
 echo
-echo -e "${S} ${C}Definir a fonte do ambiente live...${F}"
-setfont ter-122n
+echo -e "${S} ${C}Definir a fonte do ambiente live${F}"
+setfont ter-c20b
 
 # Definir o idioma do ambiente live
 echo
-echo -e "${S} ${C}Definir o idioma do ambiente live...${F}"
+echo -e "${S} ${C}Definir o idioma do ambiente live${F}"
 sed -i 's/#pt_BR.UTF-8/pt_BR.UTF-8/' /etc/locale.gen
 locale-gen
 export LANG=pt_BR.UTF-8
 
 # Atualizar o relógio do sistema
 echo
-echo -e "${S} ${C}Atualizar o relógio do sistema...${F}"
+echo -e "${S} ${C}Atualizar o relógio do sistema${F}"
 timedatectl set-ntp true
 
 # Listando os discos
 echo
-echo -e "${S} ${C}Listando os discos...${F}"
+echo -e "${S} ${C}Listando os discos${F}"
 lsblk -l | grep disk
 
 # Informando o nome do seu disco
@@ -52,46 +52,46 @@ read disco
 disco=/dev/${disco}
 
 echo
-echo -e "${S} ${C}Iniciando particionamento do disco...${F}"
+echo -e "${S} ${C}Iniciando particionamento do disco${F}"
 (echo d; echo ""; echo d; echo ""; echo g; echo n; echo ""; echo ""; echo +512M; echo t; echo 1; echo n; echo ""; echo ""; echo ""; echo w) | fdisk ${disco}
 
 # Formatando partições
 echo
-echo -e "${S} ${C}Formatando as partições...${F}"
+echo -e "${S} ${C}Formatando as partições${F}"
 mkfs.fat -F32 ${disco}1
 mkfs.ext4 ${disco}2
 
 # Montando partições
 echo
-echo -e "${S} ${C}Montando as partições...${F}"
+echo -e "${S} ${C}Montando as partições${F}"
 mount ${disco}2 /mnt
 mkdir -p /mnt/boot/efi
 mount ${disco}1 /mnt/boot/efi
 
 # Listando partições
 echo
-echo -e "${S} ${C}Conferindo as partições...${F}"
+echo -e "${S} ${C}Conferindo as partições${F}"
 lsblk ${disco}
-echo -e "${S} ${C}Aperte enter para continuar...${F}"
+echo -e "${S} ${C}Aperte enter para continuar${F}"
 read
 
 # Instalando os pacotes base
 echo
-echo -e "${S} ${C}Instalando os pacotes base...${F}"
+echo -e "${S} ${C}Instalando os pacotes base${F}"
 pacstrap /mnt base base-devel linux linux-firmware
 
 # Gerando o arquivo fstab
 echo
-echo -e "${S} ${C}Gerando o arquivo fstab...${F}"
+echo -e "${S} ${C}Gerando o arquivo fstab${F}"
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Copiando o script archinstall-02.sh para /mnt
 echo
-echo -e "${S} ${C}Copiando o script install-02.sh para /mnt...${F}"
+echo -e "${S} ${C}Copiando o script install-02.sh para /mnt${F}"
 cp install-02.sh /mnt
 
 # Iniciando arch-chroot
 echo
-echo -e "${S} ${C}Iniciando arch-chroot...${F}"
+echo -e "${S} ${C}Iniciando arch-chroot${F}"
 arch-chroot /mnt ./install-02.sh
 
--- install-02.sh
+++ install-02.sh
@@ -21,10 +21,10 @@ S='\e[32;1m[+]\e[m'
 # Funções --------------------------------------------------------
 # Adicionando a senha de root
 senharoot(){
-    echo -e "${S} ${C}Digite uma senha para o root...${F}"
-    read -s passroot1
-    echo -e "${S} ${C}Repita a senha...${F}"
-    read -s passroot2
+    echo -en "${S} ${C}Digite uma senha para o root: ${F}"
+    read -s passroot1; echo
+    echo -e "${S} ${C}Repita a senha: ${F}"
+    read -s passroot2; echo
     if [[ $passroot1 -eq $passroot2 ]]
     then
         echo "root:$passroot1" | chpasswd
@@ -37,19 +37,19 @@ senharoot(){
 
 # Adicionando um usuario
 usuario(){
-    echo -e "${S} ${C}Adicionando um usuário...${F}"
+    echo -en "${S} ${C}Adicionando um usuário${F}"
     echo
-    echo -e "${S} ${C}Digite o nome do usuário...${F}"
+    echo -e "${S} ${C}Digite o nome do usuário: ${F}"
     read usuario
     useradd -m -g users -G wheel $usuario
 }
 
 # Criando a senha de usuario
 senhauser(){
-    echo -e "${S} ${C}Digite uma senha para o $usuario...${F}"
-    read -s passuser1
-    echo -e "${S} ${C}Repita a senha...${F}"
-    read -s passuser2
+    echo -en "${S} ${C}Digite uma senha: ${F}"
+    read -s passuser1; echo
+    echo -en "${S} ${C}Repita a senha: ${F}"
+    read -s passuser2; echo
     if [[ $passuser1 -eq $passuser2 ]]
     then
         echo "$usuario:$passuser1" | chpasswd
@@ -62,54 +62,54 @@ senhauser(){
 #----------------------------------------------------------------------
 
 clear
-echo -e "${S} ${C}Bem vindo a segunda parte da instalação do Arch Linux UEFI...${F}"
+echo -e "${S} ${C}Bem vindo a segunda parte da instalação do Arch Linux UEFI${F}"
 sleep 3s
 
 # Ajustando o fuso horário
 echo
-echo -e "${S} ${C}Ajustando o fuso horário...${F}"
+echo -e "${S} ${C}Ajustando o fuso horário${F}"
 ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 
 # Executando hwclock
 echo
-echo -e "${S} ${C}Executando o hwclock...${F}"
+echo -e "${S} ${C}Executando o hwclock${F}"
 hwclock --systohc
 
 # Definindo o idioma
 echo
-echo -e "${S} ${C}Definindo o idioma...${F}"
+echo -e "${S} ${C}Definindo o idioma${F}"
 sed -i 's/#pt_BR.UTF-8/pt_BR.UTF-8/' /etc/locale.gen
 locale-gen
 
 # Criando o arquivo locale.conf
 echo
-echo -e "${S} ${C}Criando o arquivo locale.conf...${F}"
+echo -e "${S} ${C}Criando o arquivo locale.conf${F}"
 echo LANG=pt_BR.UTF-8 > /etc/locale.conf
 
 # Exportando a variável LANG
 echo
-echo -e "${S} ${C}Exportando a variável LANG...${F}"
+echo -e "${S} ${C}Exportando a variável LANG${F}"
 export LANG=pt_BR.UTF-8
 
 # Atualizando o relógio do sistema
 echo
-echo -e "${S} ${C}Atualizando o relógio do sistema...${F}"
+echo -e "${S} ${C}Atualizando o relógio do sistema${F}"
 timedatectl set-ntp true
 
 # Criando o arquivo vconsole.conf
 echo
-echo -e "${S} ${C}Criando o arquivo vconsole.conf...${F}"
+echo -e "${S} ${C}Criando o arquivo vconsole.conf${F}"
 echo KEYMAP=br-abnt2 > /etc/vconsole.conf
-echo FONT=ter-120n >> /etc/vconsole.conf
+echo FONT=ter-c20b >> /etc/vconsole.conf
 
 # Criando o hostname
 echo
-echo -e "${S} ${C}Criando o hostname...${F}"
+echo -e "${S} ${C}Criando o hostname${F}"
 echo archlinux > /etc/hostname
 
 # Configurando o hosts
 echo
-echo -e "${S} ${C}Configurando o arquivo hosts...${F}"
+echo -e "${S} ${C}Configurando o arquivo hosts${F}"
 cat >> '/etc/hosts' << EOF
 127.0.0.1   localhost.localdomain localhost
 ::1         localhost.localdomain localhost
@@ -118,56 +118,56 @@ EOF
 
 # Initramfs
 echo
-echo -e "${S} ${C}Criando um novo initramfs...${F}"
+echo -e "${S} ${C}Criando um novo initramfs${F}"
 mkinitcpio -P
 
 # Criando senha de root
 echo
-echo -e "${S} ${C}Criando a senha do root...${F}"
+echo -e "${S} ${C}Criando a senha do root${F}"
 echo
 senharoot
 
 # Baixando o Gerenciador de boot
 echo
-echo -e "${S} ${C}Baixando o Gerenciador de boot e mais alguns pacotes...${F}"
+echo -e "${S} ${C}Baixando o Gerenciador de boot e mais alguns pacotes${F}"
 pacman -S dialog dosfstools efibootmgr git grub linux-headers mtools networkmanager network-manager-applet terminus-font vim wget xorg intel-ucode --noconfirm
 
 # Instalando o grub
 echo
-echo -e "${S} ${C}Instalando o grub...${F}"
+echo -e "${S} ${C}Instalando o grub${F}"
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 
 # Configurando o grub
 echo                                                    
-echo -e "${S} ${C}Configurando o grub...${F}"
+echo -e "${S} ${C}Configurando o grub${F}"
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # Iniciando o NetworkManager
 echo
-echo -e "${S} ${C}Iniciando os Serviços NetworkManager...${F}"
+echo -e "${S} ${C}Iniciando os Serviços NetworkManager${F}"
 systemctl enable NetworkManager
 systemctl start NetworkManager
 
 # Criando usuario e senha
 echo
-echo -e "${S} ${C}Criando usuário e senha...${F}"
+echo -e "${S} ${C}Criando usuário e senha${F}"
 echo
 usuario
 senhauser
 
 # Adicionando user no grupo sudoers
 echo
-echo -e "${S} ${C}Adicionando o $usuario no grupo sudoers...${F}"
+echo -e "${S} ${C}Adicionando o $usuario no grupo sudoers${F}"
 sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
 # Colorindo a sída do pacman
 echo
-echo -e "${S} ${C}Colorindo a saída do pacman...${F}"
+echo -e "${S} ${C}Colorindo a saída do pacman${F}"
 sed -i 's/#Color/Color/' /etc/pacman.conf
 
 # Definindo lauout do teclado para pt-br
 echo
-echo -e "${S} ${C}Definindo o layout do teclado no xorg...${F}"
+echo -e "${S} ${C}Definindo o layout do teclado no xorg${F}"
 cat >> '/etc/X11/xorg.conf.d/10-keyboard.conf' << EOF
 Section "InputClass"
 Identifier "keyboard default"
