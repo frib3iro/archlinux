9927c48b6c08a314da85330da820adb32db97608
FABIO RIBEIRO <37555641+frib3iro@users.noreply.github.com>
2022-04-01 14:13:03 -0300

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -26,14 +26,10 @@ echo -e "${S} ${B}Atualizando...${F}"
 sudo pacman -Syu --noconfirm
 
 # Lista de pacotes para instalar com o pacman
-listapacman=(go fdupes firefox firefox-i18n-pt-br arc-gtk-theme alsa-utils dconf-editor gnome-passwordsafe gnome-sound-recorder archlinux-keyring archlinux-wallpaper bash-completion bluez bluez-utils cmatrix cronie cups dialog dosfstools efibootmgr git gnome-tweaks gnupg gst-libav gufw htop libreoffice libreoffice-fresh-pt-br lolcat man-db man-pages-pt_br mesa-demos mtools neofetch os-prober pass rsync qbittorrent speedtest-cli ufw unrar xclip youtube-dl pulseaudio pulseaudio-bluetooth lollypop usbutils ttf-fantasque-sans-mono ttf-jetbrains-mono ttf-dejavu ttf-hack ttf-jetbrains-mono gnome-desktop virtualbox)
-
-listahacker=(aircrack-ng wifite tcpdump bully reaver hcxdumptool hcxtools termshark cowpatty hashcat)
-
-listahackeryay=(pyrit)
+listapacman=(go fdupes firefox firefox-i18n-pt-br arc-gtk-theme alsa-utils dconf-editor gnome-passwordsafe gnome-sound-recorder archlinux-keyring archlinux-wallpaper bash-completion bluez bluez-utils cmatrix cronie cups dialog dosfstools efibootmgr git gnome-tweaks gnupg gst-libav gufw htop libreoffice libreoffice-fresh-pt-br lolcat man-db man-pages-pt_br mesa-demos mtools neofetch os-prober pass rsync qbittorrent speedtest-cli ufw unrar xclip youtube-dl pulseaudio pulseaudio-bluetooth lollypop usbutils ttf-fantasque-sans-mono ttf-jetbrains-mono ttf-dejavu ttf-hack ttf-jetbrains-mono gnome-desktop virtualbox aircrack-ng wifite tcpdump bully reaver hcxdumptool hcxtools termshark cowpatty hashcat)
 
 # Lista de pacotes para instalar com o yay
-listayay=(aic94xx-firmware consolas-font wd719x-firmware upd72020x-fw mintstick downgrade timeshift crunch gnome-terminal-transparency cava ventoy-bin mint-y-icons sardi-icons ant-dracula-theme-git debtap spotify ttf-ms-fonts ttf-roboto ttf-ubuntu-font-family onedriver)
+listayay=(aic94xx-firmware consolas-font pyrit chrome-gnome-shell wd719x-firmware upd72020x-fw mintstick downgrade timeshift crunch gnome-terminal-transparency cava ventoy-bin mint-y-icons sardi-icons ant-dracula-theme-git debtap spotify ttf-ms-fonts ttf-roboto ttf-ubuntu-font-family onedriver)
 
 # Instalando pacotes do gnome
 listagnome=(baobab cheese eog evince file-roller gdm gnome-backgrounds gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-color-manager gnome-control-center gnome-disk-utility gnome-documents gnome-font-viewer gnome-getting-started-docs gnome-keyring gnome-logs gnome-menus gnome-photos gnome-remote-desktop gnome-screenshot gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-system-monitor gnome-themes-extra gnome-user-docs gnome-user-share gnome-video-effects gnome-weather grilo-plugins gvfs gvfs-afc gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb mutter nautilus networkmanager orca rygel sushi totem tracker tracker-miners tracker3 tracker3-miners vino yelp simple-scan)
@@ -51,26 +47,6 @@ done
 echo
 echo -e "${S} ${B}Instalando pacotes do gnome...${F}"
 for i in ${listagnome[@]}
-do
-    echo -e "${S} ${B}Instalando o pacote $i ${F}"
-    sudo pacman -S $i --noconfirm
-    echo -e "${S} ${G}Pacote $i instalado com sucesso!!${F}"
-    echo
-done
-
-echo
-echo -e "${S} ${B}Instalando pacotes Hacker${F}"
-for i in ${listahacker[@]}
-do
-    echo -e "${S} ${B}Instalando o pacote $i ${F}"
-    sudo pacman -S $i --noconfirm
-    echo -e "${S} ${G}Pacote $i instalado com sucesso!!${F}"
-    echo
-done
-
-echo
-echo -e "${S} ${B}Instalando pacotes Hacker${F}"
-for i in ${listahackeryay[@]}
 do
     echo -e "${S} ${B}Instalando o pacote $i ${F}"
     sudo pacman -S $i --noconfirm
