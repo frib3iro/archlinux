29f8ce79124057751b252e199822e11ced17b9c3
Fabio Junior Ribeiro <rib3iro@live.com>
2022-04-09 14:49:04 -0300

    2.0

--- install-01.sh
+++ install-01.sh
@@ -19,15 +19,18 @@ clear
 
 # Tela de boas vindas
 echo -e "${S} ${B}Bem vindo a primeira parte da instalação do ArchLinux UEFI/GPT${F}"
+sleep 2s
 
 # Definir a fonte do ambiente live
 echo
 echo -e "${S} ${B}Definir a fonte do ambiente live${F}"
+sleep 2s
 setfont ter-120n
 
 # Definir o idioma do ambiente live
 echo
 echo -e "${S} ${B}Definir o idioma do ambiente live${F}"
+sleep 2s
 sed -i 's/#pt_BR.UTF-8/pt_BR.UTF-8/' /etc/locale.gen
 locale-gen
 export LANG=pt_BR.UTF-8
@@ -35,32 +38,38 @@ export LANG=pt_BR.UTF-8
 # Atualizar o relógio do sistema
 echo
 echo -e "${S} ${B}Atualizar o relógio do sistema${F}"
+sleep 2s
 timedatectl set-ntp true
 
 # Listando os discos
 echo
 echo -e "${S} ${B}Listando os discos${F}"
+sleep 2s
 lsblk -l | grep disk
 
 # Informando o nome do seu disco
 echo
 echo -en "${S} ${Y}Informe o nome do seu disco:${F} "
+sleep 2s
 read disco
 disco=/dev/${disco}
 
 echo
 echo -e "${S} ${B}Iniciando particionamento do disco${F}"
+sleep 2s
 (echo d; echo ""; echo d; echo ""; echo g; echo n; echo ""; echo ""; echo +512M; echo t; echo 1; echo n; echo ""; echo ""; echo ""; echo w) | fdisk ${disco}
 
 # Formatando partições
 echo
 echo -e "${S} ${B}Formatando as partições${F}"
+sleep 2s
 mkfs.fat -F32 ${disco}1
 mkfs.ext4 ${disco}2
 
 # Montando partições
 echo
 echo -e "${S} ${B}Montando as partições${F}"
+sleep 2s
 mount ${disco}2 /mnt
 mkdir -p /mnt/boot/efi
 mount ${disco}1 /mnt/boot/efi
@@ -68,6 +77,7 @@ mount ${disco}1 /mnt/boot/efi
 # Listando partições
 echo
 echo -e "${S} ${B}Conferindo as partições${F}"
+sleep 2s
 lsblk ${disco}
 echo -e "${S} ${Y}Aperte enter para continuar${F}"
 read
@@ -75,20 +85,24 @@ read
 # Instalando os pacotes base
 echo
 echo -e "${S} ${B}Instalando os pacotes base${F}"
+sleep 2s
 pacstrap /mnt base base-devel linux linux-firmware intel-ucode
 
 # Gerando o arquivo fstab
 echo
 echo -e "${S} ${B}Gerando o arquivo fstab${F}"
+sleep 2s
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Copiando o script archinstall-02.sh para /mnt
 echo
 echo -e "${S} ${B}Copiando os de instalação scripts para /mnt${F}"
+sleep 2s
 mv *.sh /mnt
 
 # Iniciando arch-chroot
 echo
 echo -e "${S} ${B}Iniciando arch-chroot${F}"
+sleep 2s
 arch-chroot /mnt ./install-02.sh
 
--- install-02.sh
+++ install-02.sh
@@ -19,53 +19,62 @@ source funcoes.sh
 clear
 
 echo -e "${S} ${B}Bem vindo a segunda parte da instalação do ArchLinux UEFI/GPT${F}"
-sleep 3s
+sleep 2s
 
 # Ajustando o fuso horário
 echo
 echo -e "${S} ${B}Ajustando o fuso horário${F}"
+sleep 2s
 ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 
 # Executando hwclock
 echo
 echo -e "${S} ${B}Executando o hwclock${F}"
+sleep 2s
 hwclock --systohc
 
 # Definindo o idioma
 echo
 echo -e "${S} ${B}Definindo o idioma${F}"
+sleep 2s
 sed -i 's/#pt_BR.UTF-8/pt_BR.UTF-8/' /etc/locale.gen
 locale-gen
 
 # Criando o arquivo locale.conf
 echo
 echo -e "${S} ${B}Criando o arquivo locale.conf${F}"
+sleep 2s
 echo LANG=pt_BR.UTF-8 > /etc/locale.conf
 
 # Exportando a variável LANG
 echo
 echo -e "${S} ${B}Exportando a variável LANG${F}"
+sleep 2s
 export LANG=pt_BR.UTF-8
 
 # Atualizando o relógio do sistema
 echo
 echo -e "${S} ${B}Atualizando o relógio do sistema${F}"
+sleep 2s
 timedatectl set-ntp true
 
 # Criando o arquivo vconsole.conf
 echo
 echo -e "${S} ${B}Criando o arquivo vconsole.conf${F}"
+sleep 2s
 echo KEYMAP=br-abnt2 > /etc/vconsole.conf
 echo FONT=ter-120n >> /etc/vconsole.conf
 
 # Criando o hostname
 echo
 echo -e "${S} ${B}Criando o hostname${F}"
+sleep 2s
 echo archlinux > /etc/hostname
 
 # Configurando o hosts
 echo
 echo -e "${S} ${B}Configurando o arquivo hosts${F}"
+sleep 2s
 cat >> '/etc/hosts' << EOF
 127.0.0.1   localhost.localdomain localhost
 ::1         localhost.localdomain localhost
@@ -75,38 +84,45 @@ EOF
 # Initramfs
 echo
 echo -e "${S} ${B}Criando um novo initramfs${F}"
+sleep 2s
 mkinitcpio -P
 
 # Criando senha de root
 echo
 echo -e "${S} ${B}Criando a senha do root${F}"
+sleep 2s
 echo
 senharoot
 
 # Baixando o Gerenciador de boot
 echo
 echo -e "${S} ${B}Baixando o Gerenciador de boot e mais alguns pacotes${F}"
-pacman -S dialog dosfstools efibootmgr git grub linux-headers mtools networkmanager network-manager-applet nm-connection-editor pulseaudio pavucontrol terminus-font vim wget os-prober xorg intel-ucode xdg-utils xdg-user-dirs-gtk --noconfirm
+sleep 2s
+pacman -S dialog dosfstools efibootmgr git grub intel-ucode linux-headers mtools networkmanager network-manager-applet nm-connection-editor os-prober pavucontrol pulseaudio terminus-font vim wget xdg-user-dirs xdg-utils xorg --noconfirm
 
 # Instalando o grub
 echo
 echo -e "${S} ${B}Instalando o grub${F}"
+sleep 2s
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 
 # Configurando o grub
-echo                                                    
+echo
 echo -e "${S} ${B}Configurando o grub${F}"
+sleep 2s
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # Iniciando o NetworkManager
 echo
 echo -e "${S} ${B}Iniciando os Serviços NetworkManager${F}"
+sleep 2s
 systemctl enable NetworkManager
 systemctl start NetworkManager
 
 # Criando usuario e senha
 echo
 echo -e "${S} ${B}Criando usuário e senha${F}"
+sleep 2s
 echo
 usuario
 senhauser
@@ -114,24 +130,28 @@ senhauser
 # Adicionando user no grupo sudoers
 echo
 echo -e "${S} ${B}Adicionando o $usuario no grupo sudoers${F}"
+sleep 2s
 sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
 # Colorindo a sída do pacman
 echo
 echo -e "${S} ${B}Colorindo a saída do pacman${F}"
+sleep 2s
 sed -i 's/#Color/Color/' /etc/pacman.conf
 
 # xdg-user-dirs
 echo
 echo -e "${S} ${B}Iniciando o xdg-update...${F}"
+sleep 2s
 xdg-user-dirs-update
 
 # Mudando o layout do teclado no xorg
 echo
 echo -e "${S} ${B}Definindo o layout do teclado no xorg${F}"
+sleep 2s
 cat >> '/etc/X11/xorg.conf.d/10-keyboard.conf' << EOF
 Section "InputClass"
-    Identifier "keyboard default"                                               
+    Identifier "keyboard default"
     MatchIsKeyboard "yes"
     Option "XkbLayout" "br"
     Option "XkbVariant" "abnt2"
@@ -141,4 +161,5 @@ EOF
 # Instalação finalizada
 echo
 echo -e "${S} ${R}Instalação finalizada!${F}"
+sleep 2s
 exit
