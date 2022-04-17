6699292597de28ef12b28a6a20e5e8e098f4ee0b
FABIO RIBEIRO <37555641+frib3iro@users.noreply.github.com>
2022-04-16 22:31:15 -0300

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -13,80 +13,76 @@
 
 source variaveis.sh
 source funcoes.sh
-source listas.sh
 
 # --------------------------------------------------------
 
 clear
 # Tela de boas vindas
 echo -e "${S} ${B}Bem vindo a instalação do gnome${F}"
-sleep 2s
 
 # Atualizando os espelhos
 echo
 echo -e "${S} ${B}Atualizando...${F}"
-sleep 2s
 sudo pacman -Syu --noconfirm
+
+# Lista de pacotes para instalar com o pacman
+listapacman=(aircrack-ng wifite tcpdump bully reaver hcxdumptool hcxtools termshark cowpatty hashcat fdupes firefox firefox-i18n-pt-br arc-gtk-theme alsa-utils dconf-editor gnome-passwordsafe gnome-sound-recorder archlinux-keyring archlinux-wallpaper bash-completion bluez bluez-utils cmatrix cronie dialog dosfstools efibootmgr git gnome-tweaks gnupg gst-libav gufw htop libreoffice libreoffice-fresh-pt-br lolcat man-db man-pages-pt_br mesa-demos mtools neofetch os-prober pass rsync qbittorrent speedtest-cli ufw unrar xclip youtube-dl pulseaudio pulseaudio-bluetooth lollypop usbutils ttf-fantasque-sans-mono ttf-jetbrains-mono ttf-dejavu ttf-hack ttf-jetbrains-mono gnome-desktop virtualbox)
+
+# Lista de pacotes para instalar com o yay
+listaparu=(xcursor-breeze pyrit aic94xx-firmware consolas-font wd719x-firmware upd72020x-fw mintstick downgrade timeshift crunch gnome-terminal-transparency cava mint-y-icons sardi-icons ant-dracula-theme-git debtap spotify ttf-ms-fonts ttf-roboto ttf-ubuntu-font-family onedriver)
+
+# Instalando pacotes do gnome
+listagnome=(baobab cheese eog evince file-roller gdm gnome-backgrounds gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-color-manager gnome-control-center gnome-disk-utility gnome-documents gnome-font-viewer gnome-getting-started-docs gnome-keyring gnome-logs gnome-menus gnome-photos gnome-remote-desktop gnome-screenshot gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-system-monitor gnome-themes-extra gnome-user-docs gnome-user-share gnome-video-effects gnome-weather grilo-plugins gvfs gvfs-afc gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb mutter nautilus networkmanager orca rygel sushi totem tracker tracker-miners tracker3 tracker3-miners vino yelp simple-scan)
 
 echo
 echo -e "${S} ${B}Instalando pacotes com pacman...${F}"
-sleep 2s
+echo
 for i in ${listapacman[@]}
 do
     echo -e "${S} ${B}Instalando o pacote $i ${F}"
-    sleep 2s
     sudo pacman -S $i --noconfirm
     echo -e "${S} ${G}Pacote $i instalado com sucesso!!${F}"
-    sleep 2s
     echo
 done
 
 echo
 echo -e "${S} ${B}Instalando pacotes do gnome...${F}"
-sleep 2s
+echo
 for i in ${listagnome[@]}
 do
     echo -e "${S} ${B}Instalando o pacote $i ${F}"
-    sleep 2s
     sudo pacman -S $i --noconfirm
     echo -e "${S} ${G}Pacote $i instalado com sucesso!!${F}"
-    sleep 2s
     echo
 done
 
-# Instalando o yay
+# Instalando o paru
 echo
-echo -e "${S} ${B}Instalando o yay......${F}"
-sleep 2s
-git clone https://aur.archlinux.org/yay.git
-cd yay
+echo -e "${S} ${B}Instalando o paru...${F}"
+git clone https://aur.archlinux.org/paru.git
+cd paru
 makepkg -si --noconfirm
 
 echo
-echo -e "${S} ${B}Instalando pacotes com yay...${F}"
-sleep 2s
-for i in ${listayay[@]}
+echo -e "${S} ${B}Instalando pacotes com paru...${F}"
+echo
+for i in ${listaparu[@]}
 do
     echo -e "${S} ${B}Instalando o pacote $i ${F}"
-    sleep 2s
-    yay -S $i --noconfirm
+    paru -S $i --noconfirm
     echo -e "${S} ${G}Pacote $i instalado com sucesso!!${F}"
-    sleep 2s
     echo
 done
 
 # Bluez
 echo
 echo -e "${S} ${B}Iniciando o bluez...${F}"
-sleep 2s
 sudo systemctl enable bluetooth
 sudo systemctl start bluetooth
-sudo systemctl enable org.cups.cupsd
 
 # Gnome Display Manager
 echo
 echo -e "${S} ${B}Instalando o Gnome Display Manager...${F}"
-sleep 2s
 sudo systemctl enable gdm.service
 sudo systemctl start gdm.service
 
